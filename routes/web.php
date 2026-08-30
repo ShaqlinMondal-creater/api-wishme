445150ea9fef221ac12680434d3b222b2467e705
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2026-08-30 11:09:35 +0530

    ..

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json([
-        'name' => config('wishme.name').' API',
-        'brand' => config('wishme.brand'),
-        'health' => url('/api/health'),
-    ]);
+    return view('welcome');
 });
